3c89c0a30904dde3242e6b8a8c8454986d265843
Charlotte Andry <charlotteandry123@gmail.com>
2023-11-11 10:18:52 -0600

    starter code

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>Hello World</title>
+    </head>
+    <body>
+        <form action = "process.php" method = "post">
+            Enter your name: <input name = "name" type = "text">
+            <input type = "submit">
+        </form>
+    </body>
+</html>
---
+++ process.php
@@ -0,0 +1,9 @@
+<html>
+    <head>
+        <title>Process Info</title>
+    </head>
+    <body>
+        <?php
+        ?>
+    </body>
+</html>
