71d69a9d05238f8c9696fc12f009b74caee4db9f
Charlotte Andry <charlotteandry123@gmail.com>
2023-11-24 11:09:51 -0600

    sign-up/log-in complete

--- process-sign-up.php
+++ process-sign-up.php
@@ -29,23 +29,13 @@
 
         if ($stmt->execute()) {
             echo "<br>";
-            echo "Sign-up of = $user_name is successfully";
+            echo "Sign-up of = $user_name is successfull, please log in!";
+            // TODO: add header() here to redirect them. ** doing this wont 
+            // show the "success" message
         } else {
             echo "Error adding a new user " . $stmt->error;
         }
         $stmt->close();
-
-
-/*
-        if ($result->num_rows > 0) {
-            echo "Nurses: <br><br>";
-            while($row = $result->fetch_assoc()) {
-                echo "Name: " . $row["Fname"]. " " . $row["Lname"]. "<br>";
-            }
-        } else {
-            echo "0 results";
-        }
-*/
         $conn->close();
         ?> 
     </body>
